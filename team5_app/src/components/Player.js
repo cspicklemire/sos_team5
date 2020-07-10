d1d1941cb7fd5d371c6d625d243b54badc78351c
edtorrez <etorrez2017@gmail.com>
2020-07-10 09:12:28 -0500

    player component updated

--- team5_app/src/components/Player.js
+++ team5_app/src/components/Player.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../css/Player-Comp.css';
 
-function Player() {
+function Player(props) {
+  const [player, setPlayer] = useState('')
+
+  console.log( process.env.PUBLIC_URL+'/'+player.image );
+
+  useEffect( () => {
+    setPlayer( props.data );
+  }, [])
+
   return (
     <div className='player-comp'>
-      
+      <h1> { `../img/players/${player.image}` } </h1>
+      <img source={ `../img/players/${player.image}` } className="player-logo" alt="player" />
     </div>
   );
 }
--- team5_app/src/pages/Players.js
+++ team5_app/src/pages/Players.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import Player from '../components/Player.js';
+import Data from '../json/Players.json';
 import '../css/Player.css';
 
 function Players() {
   return (
     <div className='player-page'>
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
-      <Player />
+      {Data.map( (player, index) => {
+        return <Player data={player} key={index} />
+      })}
     </div>
   );
 }
