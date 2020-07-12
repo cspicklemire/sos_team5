54c38746938aff07c0176b6c4230127bded8bf52
Clay Spicklemire <clay@clays-mbp.attlocal.net>
2020-07-11 23:03:14 -0400

    added logout button

--- team5_app/src/components/GoogleLogin.js
+++ team5_app/src/components/GoogleLogin.js
@@ -5,7 +5,7 @@ function GoogleLogin(props) {
     const signInButton = (
         <div className = 'GoogleLogin'>
             <a href="/google/login">
-            <img src = {'/img/Google_Sign_In.png'} alt = 'Google Login' width = "200" height = "50"></img>
+                <img src = {'/img/Google_Sign_In.png'} alt = 'Google Login' width = "200" height = "50"></img>
             </a>
         </div>
         )
@@ -13,7 +13,9 @@ function GoogleLogin(props) {
     const signOutButton = (
         <div className = 'GoogleLogin'>
             <span style = {{color:'white'}} >You are currently logged in as <br /> {props.email} <br /> </span>
-            <a href="/google/logout">Logout </a>
+            <a href="/google/logout"> 
+                <img src = {'/img/Google_Logout.png'} alt = 'Google Login' width = "75" height = "30"></img>
+            </a>
         </div>
         )
 
