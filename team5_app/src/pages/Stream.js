3757a6061661604ba0a6fcdaa9aa43a493837260
Clay Spicklemire <cspicklemmire@crimson.ua.edu>
2020-07-14 22:35:00 -0400

    premium chat

--- team5_app/src/pages/Stream.js
+++ team5_app/src/pages/Stream.js
@@ -2,28 +2,17 @@ import React, { useState, useEffect } from 'react'
 import UsernamePopup from '../components/UsernamePopup'
 import LoginPopup from '../components/LoginPopup'
 import Data from '../json/Videos.json';
+import Button from 'react-bootstrap/Button'
 
 function Stream( props ) {
     const [messages, setMessages] = useState([])
 	const [chatText, setChatText] = useState('')
 	const [usernameFlag, setUsernameFlag] = useState(false)
 	const [loginFlag, setLoginFlag] = useState(false)
+    const [room, setRoom] = useState('Standard')
+	const [display, setDisplay] = useState('none')
 
-
-
-
-    const postChat = (event) => {
-        if (event.key === 'Enter') {
-            props.socket.emit('message', {message: event.target.value, username: props.username})
-			setChatText('')
-        }
-    }
-
-	const handleChange = (e) => {
-		setChatText(e.target.value)
-	}
-	
-	function launchModal() { 
+    function launchModal() { 
 	    if (!props.email) {
 	        setLoginFlag(true)
 	    }
@@ -31,24 +20,54 @@ function Stream( props ) {
 	        setUsernameFlag(true)
 	    }
 	}
+	
+	function changeRoom() {
+        setRoom(oldRoom => (oldRoom == 'Standard') ? 'Premium' : 'Standard')
+        setMessages([])
+        setChatText('')
+	}
+
+    const postChat = (event) => {
+        if (event.key === 'Enter') {
+            props.socket.emit('message', {message: event.target.value, username: props.username, room: room})
+			setChatText('')
+        }
+    }
+
+
+	const handleChange = (e) => {
+		setChatText(e.target.value)
+	}
+	
+	
 
 	useEffect(() => {
 
 	    const gotMessage = (message) => {
             setMessages( m => [...m, message])
         }
-
+        props.socket.emit('join', { 'room': room, 'username' : props.username});
 		props.socket.on('message', gotMessage)
-		return (() => {
+        return (() => {
 			props.socket.removeListener('message', gotMessage)
+	        props.socket.emit('leave', { 'room': room, 'username' : props.username});
 		})
-	},[props.socket]);
+	},[props.socket, room, props.username]);
 
+    if (props.status === 'Premium' && display === 'none') {
+        setDisplay('inline')
+    }
+    
     return (
     <div className='stream-page'>
       <LoginPopup email = { props.email } loginFlag = { loginFlag } />
       <UsernamePopup username = { props.username } usernameFlag = { usernameFlag } setUsername = { props.setUsername } />
-      <h1> { `Live Video Stream ${props.match.params.id}` } </h1>
+      <h1> { `Live Video Stream ${props.match.params.id}` } 
+        <div style = {{ display: display}}>
+            <span> You are currently viewing the {room} chat </span>
+            <Button variant="info" onClick = {changeRoom}>Switch Chat</Button>
+        </div>
+      </h1>
       <div className='video-page-wrapper stream-page-wrapper'>
         <div className='video-page-background'>
           <div className='video-page-background-2'>
