2d49070530b64d58eef084b271b36191dceb0f13
edtorrez <etorrez2017@gmail.com>
2020-07-15 18:54:05 -0500

    fixed sponsor having description

--- team5_app/src/components/Sponsor.js
+++ team5_app/src/components/Sponsor.js
@@ -2,11 +2,8 @@ import React from 'react';
 
 function Sponsor(props) {
   return (
-    <div className='player-comp'>
+    <div className='player-comp sponsor-comp'>
       <img src={ `/img/sponsors/${props.data.image}` } className="player-logo" alt="player" />
-      <div className='player-bottom'>
-        <h3> { `${props.data.name}` } </h3>
-      </div>
     </div>
   );
 }
