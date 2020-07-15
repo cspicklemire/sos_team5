33da92bb47579efdd44f3c1d4921cd5b75bebb16
edtorrez <etorrez2017@gmail.com>
2020-07-15 18:49:21 -0500

    player info

--- team5_app/src/components/Player.js
+++ team5_app/src/components/Player.js
@@ -5,7 +5,14 @@ function Player(props) {
     <div className='player-comp'>
       <img src={ `/img/players/${props.data.image}` } className="player-logo" alt="player" />
       <div className='player-bottom'>
-        <h3> { `${props.data.name}` } </h3>
+        <div className='player-bottom-background'>
+          <h1 className='video-title'> { `${props.data.name}` } </h1>
+          <h2 className='video-description'> { `#${props.data.number}` } </h2>
+          <h2 className='video-description'> { `${props.data.position}` } </h2>
+          <h2 className='video-description'> { `${props.data.height}` } </h2>
+          <h2 className='video-description'> { `${props.data.weight}` } </h2>
+          <h2 className='video-description'> { `${props.data.born}` } </h2>
+        </div>
       </div>
     </div>
   );
