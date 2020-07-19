11ee474e0ad79a8dc75ec564c5d4623942bb351b
Clay Spicklemire <cspicklemmire@crimson.ua.edu>
2020-07-19 19:31:02 -0400

    DecibelMeter

---
+++ team5_app/src/components/DecibelMeter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DecibelMeter(props) {
+
+
+	return (
+		
+		<img src = '/img/DecibelMeter.png' alt = 'Decibel Meter' width = "500" height = "250" />
+	
+	)
+}
+
+export default DecibelMeter
---
+++ team5_app/src/pages/Upload.js
@@ -0,0 +1,52 @@
+import React, {useReducer, useState} from 'react';
+import DragNDrop from '../components/DragNDrop';
+import Button from 'react-bootstrap/Button';
+
+
+
+
+
+
+function Upload( props ) {
+
+	const reducer = (state, action) => {
+	  switch (action.type) {
+		case 'SET_DROP_DEPTH':
+		  return { ...state, dropDepth: action.dropDepth }
+		case 'SET_IN_DROP_ZONE':
+		  return { ...state, inDropZone: action.inDropZone };
+		case 'ADD_FILE_TO_LIST':
+		  return { ...state, fileList: state.fileList.concat(action.files) };
+		default:
+		  return state;
+  	  }
+	};
+	const [button, setButton] = useState(false)
+
+	const [data, dispatch] = useReducer(
+	  reducer, { dropDepth: 0, inDropZone: false, fileList: [] }
+	)
+	
+	const handleClick = () => document.location.reload(true)
+	
+  return (
+    <div>
+      <h1> Message/Video Upload </h1>
+      <br />
+      <br />
+      <DragNDrop data = { data } dispatch = {dispatch} setButton = {setButton} />
+      <ol className="dropped-files">
+   		 {data.fileList.map(f => {
+           return (
+              <li key={f.name}>{f.name}</li>
+           )
+          })}
+      </ol>
+      <Button variant="primary" onClick= {handleClick }  disabled = {!button}>
+      	Submit
+      </Button>
+    </div>
+  );
+}
+
+export default Upload;
