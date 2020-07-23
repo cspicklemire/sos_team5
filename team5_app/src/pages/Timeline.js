d81438f40c1b92060a64305d09579ced2763a215
edtorrez <etorrez2017@gmail.com>
2020-07-23 16:13:50 -0500

    timeline tiles w/ gap

--- team5_app/src/pages/Timeline.js
+++ team5_app/src/pages/Timeline.js
@@ -4,20 +4,118 @@ import Video from '../components/Video.js';
 function Timeline() {
   return (
     <div className='basic-page'>
-      <Video id='0' />
-      <Video id='1' />
-      <Video id='2' />
-      <Video id='3' />
-      <Video id='2' />
-      <Video id='4' />
-      <Video id='1' />
-      <Video id='1' />
-      <Video id='0' />
-      <Video id='3' />
-      <Video id='0' />
-      <Video id='4' />
+    <div className='home-flex page-flex'>
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='1' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='2' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='3' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='0' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='1' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='3' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='2' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='0' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='2' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='1' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='3' />
+          </div>
+        </div>
+      </div>
+
+      <div className='home-flex-container'>
+        <div className='home-flex-relative'>
+          <div className='home-flex-content'>
+            <Video id='0' />
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
-
+/*
+<Video id='0' />
+<Video id='1' />
+<Video id='2' />
+<Video id='3' />
+<Video id='2' />
+<Video id='4' />
+<Video id='1' />
+<Video id='1' />
+<Video id='0' />
+<Video id='3' />
+<Video id='0' />
+<Video id='4' />
+*/
 export default Timeline;
